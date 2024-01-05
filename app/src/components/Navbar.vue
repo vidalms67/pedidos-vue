ab64f98c275c518e71627a4b3c3d1b943e123132
vidalms67 <gabrieldev95@gmail.com>
2024-01-04 23:23:30 -0400

    router

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <nav id="nav">
+                <RouterLink to="/">
+                    <img src="" id="logo">
+                </RouterLink>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+<style scoped>
+
+</style>
