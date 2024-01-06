836a516baa98b4e2ac98cf99046924703da11af3
vidalms67 <gabrieldev95@gmail.com>
2024-01-05 23:26:00 -0400

    footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,11 +2,13 @@
   <div>
     <Navbar />
     <RouterView />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 import router from './router';
 
 export default{
